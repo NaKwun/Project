b14457d7aa0515f7706f39cfee9783c1dbbc34f2
NaKwun <kwunkuncarzz@gmail.com>
2020-04-26 00:17:26 +0700

    pro1

---
+++ cuuntrt.js
@@ -0,0 +1,30 @@
+const timeLine = "https://api.covid19api.com/summary";
+
+$(document).ready(function () {
+    $.get(timeLine, function (Country) {
+        $("#myTable").DataTable({
+            processing: true,
+            data: Country.Countries,
+            columns: [
+
+                { data: "Country" },
+                { data: "Date" },
+                { data: "TotalConfirmed" },
+                { data: "TotalRecovered" },
+                { data: "TotalDeaths" },
+                //   { data: "Confirmed" },
+                //   { data: "Recovered" },
+                //   { data: "Hospitalized" },
+
+            ],
+        });
+
+
+
+
+
+    }).fail(function () {
+        alert("Can not data from URL");
+    });
+});
+
